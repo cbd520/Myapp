6c7c1a93c39e73b917bae65033f2ad8bc5f1971a
huan <huan@chidadeiMac.local>
2016-07-07 16:23:13 +0800

    gaga

---
+++ Myapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Myapp
+//
+//  Created by huan on 16/7/7.
+//  Copyright © 2016年 cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Myapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Myapp
+//
+//  Created by huan on 16/7/7.
+//  Copyright © 2016年 cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
